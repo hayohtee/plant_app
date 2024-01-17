4cbec4f71a4142e8e8a96058c221fc85ebe8a256
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-01-17 16:39:56 +0100

    Create scan_page.dart

---
+++ lib/ui/screen/scan/scan_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ScanPage extends StatelessWidget {
+  const ScanPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Scan Plant'),
+      ),
+      body: const Center(
+        child: Text('Scan Page'),
+      ),
+    );
+  }
+}
