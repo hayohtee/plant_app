9a285c427de8f5589c11c4af6cef4fee5532bc8e
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-01-21 11:40:24 +0100

    Update profile_screen.dart

--- lib/ui/screen/profile/profile_screen.dart
+++ lib/ui/screen/profile/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:plant_app/ui/screen/profile/profile_header.dart';
+import 'package:plant_app/ui/screen/profile/profile_option.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,8 +8,22 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('Profile screen'),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              ProfileHeader(),
+              SizedBox(height: 16),
+              ProfileOption(label: 'My Profile', icon: Icons.person),
+              ProfileOption(label: 'Settings', icon: Icons.settings),
+              ProfileOption(label: 'Notifications', icon: Icons.notifications),
+              ProfileOption(label: 'FAQ', icon: Icons.chat),
+              ProfileOption(label: 'Share', icon: Icons.share),
+              ProfileOption(label: 'Log Out', icon: Icons.logout),
+            ],
+          ),
+        ),
       ),
     );
   }
